5b66c836988286840a40a90d7976178cca226c3c
tingxuanhu <paperplant2022@gmail.com>
2022-07-17 16:53:17 +0800

    update

---
+++ src/mergesort/MergeSort.java
@@ -0,0 +1,45 @@
+package mergesort;
+
+public class MergeSort {
+
+    public static void mergeSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    public static void process(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int m = l + ((r - l) >> 1);
+        process(arr, l, m);
+        process(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+
+    public static int[] merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m + 1;
+        while (p1 <= m && p2 <= r) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+
+    }
+
+}
