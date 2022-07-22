38c57ae50bff1efadf2dd10425c1160a5bc10a3d
tingxuanhu <paperplant2022@gmail.com>
2022-07-22 10:52:06 +0800

    update code

--- src/heap/CoverMax.java
+++ src/heap/CoverMax.java
@@ -1,20 +1,22 @@
 package heap;
 
+import java.util.Comparator;
+
 public class CoverMax {
 
     public static int getMax(int[][] lines) {
 
     }
 
-
-    public static class Line {
-        public final int start;
-        public final int end;
-
-        public Line(int start, int end) {
-            this.start = start;
-            this.end = end;
+    public static class StartComparator implements Comparator<Line> {
+        @Override
+        public int compare(Line o1, Line o2) {
+            return o1.start - o2.start;
         }
+    }
+
+    public record Line(int start, int end) {
+
     }
 
 }
