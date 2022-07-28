81bd39032c92099d0bf420e407bc828f74dbceb5
tingxuanhu <paperplant2022@gmail.com>
2022-07-28 20:16:42 +0800

    Update

--- src/datastucture/linkedlist/LinkedListMid.java
+++ src/datastucture/linkedlist/LinkedListMid.java
@@ -8,25 +8,12 @@ public class LinkedListMid {
     4)  奇数个节点返回唯一的中点的前一个节点,偶数个节点则返回后一个中点的前一个;
      */
 
-    public static class Node {
-        private int value;
-        private Node next;
-
-        public Node(int data) {
-            this.value = data;
-        }
-    }
-
-    public static Node midOrUpMidNode(Node head) {
-        if (head == null || head.next == null || head.next.next == null) {
-            return head;
-        }
-
-        // 否则的话 链表就一定超过3个点
-        Node slow = head.next;
-        Node fast = head.next.next;
-
-    }
+    // 对照草稿纸掰扯掰扯再写
 
 
 }
+
+
+
+
+
