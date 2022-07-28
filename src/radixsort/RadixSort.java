c280473e43515119b1aad1d916690d915c631591
tingxuanhu <paperplant2022@gmail.com>
2022-07-28 13:59:27 +0800

    update files

--- src/radixsort/RadixSort.java
+++ src/radixsort/RadixSort.java
@@ -1,5 +1,7 @@
 package radixsort;
 
+import java.util.Arrays;
+
 public class RadixSort {
     // only for non-negative value
     public static void radixSort(int[] arr) {
@@ -55,20 +57,74 @@ public class RadixSort {
         return ((x / (int) (Math.pow(10, d))) % 10);  //pow返回double类型 要强制转换
     }
 
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) (Math.random() * maxSize + 1)];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * (maxValue + 1));
+        }
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        System.arraycopy(arr, 0, res, 0, arr.length);
+        return res;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i : arr) {
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int testTime = 100000;
+        int maxSize = 100;
+        int maxValue = 10000;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            radixSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Finished!" : "Fucking fucked!");
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
