363c67d4f52de90ace9438d65315a6f21fd9bd9d
tingxuanhu <paperplant2022@gmail.com>
2022-07-30 19:52:55 +0800

    update file

---
+++ src/datastucture/linkedlist/FindFirstIntersectNode.java
@@ -0,0 +1,31 @@
+package datastucture.linkedlist;
+
+public class FindFirstIntersectNode {
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+
+    // 看是否有环,如果无环返回null,若有环则返回第一个入环节点
+    public static Node getLoopNode(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        Node slow = head.next;
+        Node fast = head.next.next;
+        while (slow != fast) {
+            if (fast.next == null || fast.next.next == null) {
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+    }
+
+}
