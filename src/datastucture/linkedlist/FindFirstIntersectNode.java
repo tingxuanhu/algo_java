87b9df689dd8073bef31a25f92ef36640cfa9674
burt <paperplant2022@gmail.com>
2022-07-31 00:06:56 +0800

    update file

--- src/datastucture/linkedlist/FindFirstIntersectNode.java
+++ src/datastucture/linkedlist/FindFirstIntersectNode.java
@@ -25,7 +25,53 @@ public class FindFirstIntersectNode {
             slow = slow.next;
             fast = fast.next.next;
         }
+        fast = head;
+        while (slow != fast) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    // 探查两个无环链表
+    public static Node noLoop(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;
+        while (cur1.next != null) {
+            cur1 = cur1.next;
+            n++;
+        }
+        while (cur2.next != null) {
+            cur2 = cur2.next;
+            n--;
+        }
+        // 比较两链表结尾的地址是否相等
+        if (cur1 != cur2) {
+            return null;
+        }
+
+        cur1 = n > 0 ? head1 : head2;  // 长度长的做cur1
+        cur2 = cur1 == head1 ? head2 : head1;
+        n = Math.abs(n);
+        while (n > 0) {
+            cur1 = cur1.next;
+            n--;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+
+    // 探查两个有环链表
+    public static Node bothLoop(Node head1, Node loop1, Node head2, Node loop2) {
 
     }
+
 
 }
