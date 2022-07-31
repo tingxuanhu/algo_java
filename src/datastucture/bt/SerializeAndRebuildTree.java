3acbfe32414670ac8716068440a35cffcc7ed5c6
burt <paperplant2022@gmail.com>
2022-07-31 12:27:38 +0800

    update file

---
+++ src/datastucture/bt/SerializeAndRebuildTree.java
@@ -0,0 +1,15 @@
+package datastucture.bt;
+
+public class SerializeAndRebuildTree {
+    public static class Node {
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+
+}
