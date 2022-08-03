8437b663f1e138abfa647144af7dde604b733a2f
burt <paperplant2022@gmail.com>
2022-08-03 23:34:24 +0800

    update file

--- src/datastucture/bt/recursion/MaxSubBSTSize.java
+++ src/datastucture/bt/recursion/MaxSubBSTSize.java
@@ -4,13 +4,21 @@ package datastucture.bt.recursion;
 public class MaxSubBSTSize {
 
     public static class Node {
-        public int value;
+        public int val;
         public Node left;
         public Node right;
 
         public Node(int data) {
-            this.value = data;
+            this.val = data;
         }
+    }
+
+    // 树型dp 二叉树递归套路  分两类讨论  与x节点有关和与x节点无关
+    public static int maxSubBSTRecursion(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        return process(head).maxBSTSubtreeSize;
     }
 
     public static class Info {
@@ -28,7 +36,46 @@ public class MaxSubBSTSize {
     }
 
     public static Info process(Node x) {
+        if (x == null) {
+            return null;
+        }
+        Info leftInfo = process(x.left);
+        Info rightInfo = process(x.right);
+        int max = x.val;
+        int min = x.val;
+        int allSize = 1;
+        if (leftInfo != null) {
+            max = Math.max(max, leftInfo.max);
+            min = Math.min(min, leftInfo.min);
+            allSize += leftInfo.allSize;
+        }
+        if (rightInfo != null) {
+            max = Math.max(max, rightInfo.max);
+            min = Math.max(min, rightInfo.min);
+            allSize += rightInfo.allSize;
+        }
 
+        int p1 = -1;
+        if (leftInfo != null) {
+            p1 = leftInfo.maxBSTSubtreeSize;
+        }
+        int p2 = -1;
+        if (rightInfo != null) {
+            p2 = rightInfo.maxBSTSubtreeSize;
+        }
+        int p3 = -1;
+        boolean leftBST = leftInfo == null || (leftInfo.maxBSTSubtreeSize == leftInfo.allSize);
+        boolean rightBST = rightInfo == null || (rightInfo.maxBSTSubtreeSize == rightInfo.allSize);
+        if (leftBST && rightBST) {
+            boolean leftMaxLessX = leftInfo == null || leftInfo.max < x.val;
+            boolean rightMaxMoreX = rightInfo == null || rightInfo.min > x.val;
+            if (leftMaxLessX && rightMaxMoreX) {
+                int leftSize = leftInfo == null ? 0 : leftInfo.allSize;
+                int rightSize = rightInfo == null ? 0 : rightInfo.allSize;
+                p3 = leftSize + rightSize + 1;
+            }
+        }
+        return new Info(Math.max(p1, Math.max(p2, p3)), allSize, max, min);
     }
 
 
