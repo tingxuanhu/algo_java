0ba8cb9e07b637a3bfe0f69d5e8ae18ec9ed89bb
tingxuanhu <paperplant2022@gmail.com>
2022-08-03 21:01:12 +0800

    Update code

--- src/datastucture/bt/recursion/IsFullBT.java
+++ src/datastucture/bt/recursion/IsFullBT.java
@@ -1,6 +1,34 @@
 package datastucture.bt.recursion;
 
 public class IsFullBT {
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
 
+        public Node(int data) {
+            val = data;
+        }
+    }
+
+    public static class Info {
+        public int numOfNodes;
+        public int height;
+
+        public Info(int num, int h) {
+            numOfNodes = num;
+            height = h;
+        }
+    }
+
+    public static Info process(Node x) {
+        if (x == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = process(x.left);
+        Info rightInfo = process(x.right);
+
+
+    }
 
 }
