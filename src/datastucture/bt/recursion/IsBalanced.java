7385defa21a048b17db66eb7c331c24c711a982a
tingxuanhu <paperplant2022@gmail.com>
2022-08-03 10:57:01 +0800

    update file

--- src/datastucture/bt/recursion/IsBalanced.java
+++ src/datastucture/bt/recursion/IsBalanced.java
@@ -1,5 +1,99 @@
 package datastucture.bt.recursion;
 
-
+/*
+二叉树是平衡二叉树需要满足的递归条件:
+1) 左子树是平衡二叉树
+2) 右子树是平衡二叉树
+3) 左右子树高度差不超过2
+ */
 public class IsBalanced {
+
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.val = data;
+        }
+    }
+
+    public static boolean isBalancedRecursion(Node head) {
+        return process(head).isBalanced;
+    }
+
+    // 理论上只需要左树和右树是否平衡的信息 但因为递归 平衡信息需要附带高度信息才能玩转
+    // 而递归对所有入参都要提出相同的要求 因此要向左子树和右子树都要求平衡信息和高度信息
+    public static class Info {
+        public boolean isBalanced;
+        public int height;
+
+        public Info(boolean isBalanced, int height) {
+            this.isBalanced = isBalanced;
+            this.height = height;
+        }
+    }
+
+    public static Info process(Node x) {
+        if (x == null) {
+            return new Info(true, 0);
+        }
+        Info leftInfo = process(x.left);
+        Info rightInfo = process(x.right);
+
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        boolean isBalanced = leftInfo.isBalanced && rightInfo.isBalanced;
+        if (Math.abs(leftInfo.height - rightInfo.height) > 1) {
+            isBalanced = false;
+        }
+        return new Info(isBalanced, height);
+    }
+
+    public static boolean isBalanced1(Node head) {
+        boolean[] ans = new boolean[1];
+        ans[0] = true;
+        process1(head, ans);
+        return ans[0];
+    }
+
+    public static int process1(Node head, boolean[] ans) {
+        if (!ans[0] || head == null) {
+            return -1;
+        }
+        int leftHeight = process1(head.left, ans);
+        int rightHeight = process1(head.right, ans);
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            ans[0] = false;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 5;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (isBalanced1(head) != isBalancedRecursion(head)) {
+                System.out.println("Oops");
+            }
+        }
+        System.out.println("Finished!");
+    }
+
 }
