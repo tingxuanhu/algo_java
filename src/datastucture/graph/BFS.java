935e796f196c1cce6d76fa9eeec7d8bf224afbd1
tingxuanhu <paperplant2022@gmail.com>
2022-08-07 17:13:37 +0800

    update file

---
+++ src/datastucture/graph/BFS.java
@@ -0,0 +1,31 @@
+package datastucture.graph;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// 思考如何用栈实现bfs ？
+
+public class BFS {
+
+    public static void bfs(Node start) {
+        if (start == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        HashSet<Node> set = new HashSet<>();
+        queue.add(start);
+        set.add(start);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            System.out.println(cur.value);
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    queue.add(next);
+                    set.add(next);
+                }
+            }
+        }
+    }
+
+}
