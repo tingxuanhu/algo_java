8ea93ca3d8c93509830d17300c7ab24b8e7a2b77
tingxuanhu <paperplant2022@gmail.com>
2022-08-07 11:48:22 +0800

    update file

---
+++ src/datastucture/graph/Node.java
@@ -0,0 +1,20 @@
+package datastucture.graph;
+
+import java.util.ArrayList;
+
+public class Node {
+
+    public int value;
+    public int in;
+    public int out;
+    public ArrayList<Node> nexts;  // 从当前node出发能找到的直接邻居
+    public ArrayList<Edge> edges;  // 从当前node出发能找到的直接的边
+
+    public Node(int value) {
+        this.value = value;
+        in = 0;
+        out = 0;
+        nexts = new ArrayList<>();
+        edges = new ArrayList<>();
+    }
+}
