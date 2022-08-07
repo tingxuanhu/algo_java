6f72da30078e57662037529863c21a3bbc1c24d9
tingxuanhu <paperplant2022@gmail.com>
2022-08-07 16:55:23 +0800

    update file

--- src/datastucture/graph/unionset/NumberOfIslandsII.java
+++ src/datastucture/graph/unionset/NumberOfIslandsII.java
@@ -1,9 +1,11 @@
 package datastucture.graph.unionset;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 // 动态+1 动态初始化的技巧  动态空降1下来
+// class15
 public class NumberOfIslandsII {
 
     public static List<Integer> numIslands2(int m, int n, int[][] positions) {
@@ -16,13 +18,13 @@ public class NumberOfIslandsII {
     }
 
     public static class UnionFind {
-        private int[] parent;
+        private final int[] parent;
         // size[i]被合并之后 不清除掉 用来标记 (size[i] != 0表示被初始化过)
-        private int[] size;
-        private int[] help;
+        private final int[] size;
+        private final int[] help;
         private int sets;
-        private int row;
-        private int col;
+        private final int row;
+        private final int col;
 
         public UnionFind(int m, int n) {
             row = m;
@@ -89,13 +91,79 @@ public class NumberOfIslandsII {
         private int index(int r, int c) {
             return r * col + c;
         }
-
     }
 
-
-
     // m,n 很大的时候 (17, 1009) --> '17_1009' 代表
+    // k个数空降成1 复杂度是O(k) 当k很小的时候事实上可以不用那么多的空间 动态动用就可以 写法如下所示
+    public static List<Integer> numIslands2dynamic(int m, int n, int[][] positions) {
+        UnionFindDynamic uf = new UnionFindDynamic();
+        List<Integer> ans = new ArrayList<>();
+        for (int[] position : positions) {
+            ans.add(uf.connect(position[0], position[1]));
+        }
+        return ans;
+    }
 
+    public static class UnionFindDynamic {
+        private final HashMap<String, String> parent;
+        private final HashMap<String, Integer> size;
+        private final ArrayList<String> help;
+        private int sets;
+
+        public UnionFindDynamic() {
+            parent = new HashMap<>();
+            size = new HashMap<>();
+            help = new ArrayList<>();
+            sets = 0;
+        }
+
+        public int connect(int r, int c) {
+            String key = String.valueOf(r) + "_" + String.valueOf(c);
+            if (!parent.containsKey(key)) {
+                parent.put(key, key);
+                size.put(key, 1);
+                sets++;
+                String up = String.valueOf(r - 1) + "_" + String.valueOf(c);
+                String down = String.valueOf(r + 1) + "_" + String.valueOf(c);
+                String left = String.valueOf(r) + "_" + String.valueOf(c - 1);
+                String right = String.valueOf(r) + "_" + String.valueOf(c + 1);
+                union(up, key);
+                union(down, key);
+                union(left, key);
+                union(right, key);
+            }
+            return sets;
+        }
+
+        private void union(String s1, String s2) {
+            if (parent.containsKey(s1) && parent.containsKey(s2)) {
+                String f1 = find(s1);
+                String f2 = find(s2);
+                if (!f1.equals(f2)) {
+                    if (size.get(f1) >= size.get(f2)) {
+                        parent.put(f2, f1);
+                        size.put(f1, size.get(f1) + size.get(f2));
+                    } else {
+                        parent.put(f1, f2);
+                        size.put(f2, size.get(f1) + size.get(f2));
+                    }
+                    sets--;
+                }
+            }
+        }
+
+        private String find(String cur) {
+            while (!cur.equals(parent.get(cur))) {
+                help.add(cur);  // --> key
+                cur = parent.get(cur);
+            }
+            for (String key : help) {
+                parent.put(key, cur);
+            }
+            help.clear();
+            return cur;
+        }
+    }
 
 
 }
