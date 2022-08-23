ebf8a28b84282ff8ac2410070282389ca684be27
tingxuanhu <paperplant2022@gmail.com>
2022-08-23 13:58:12 +0800

    update file

---
+++ src/manacher/AddShortestEnd.java
@@ -0,0 +1,7 @@
+package manacher;
+
+// 在后边添加最少的字符使得原来字符串回文
+public class AddShortestEnd {
+
+
+}
--- src/manacher/Manacher.java
+++ src/manacher/Manacher.java
@@ -27,11 +27,22 @@ package manacher;
 
 public class Manacher {
 
+    // "12321" --> "#1#2#3#2#1#"
+    public static char[] manacherString(String s) {
+        char[] charArr = s.toCharArray();
+        char[] res = new char[s.length() * 2 + 1];
+        int index = 0;
+        for (int i = 0; i < res.length; i++) {
+            res[i] = (i & 1) == 0 ? '#' : charArr[index++];
+        }
+        return res;
+    }
+
     public static int manacher(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        char[] str = manacherString(s);   // "12321" --> "#1#2#3#2#1#"
+        char[] str = manacherString(s);
         // 记录回文半径大小
         int[] pArr = new int[str.length];
         // 记录pArr最大值  --> 最大值减去1就会对应到究竟能有多长
@@ -40,7 +51,7 @@ public class Manacher {
         int R = -1;
         for (int i = 0; i < str.length; i++) {
             // 设置不用验证的区域              2 * C - i --> i'
-            pArr[i] = R > i ? Math.min(pArr[2 * C - i], R - i): 1;
+            pArr[i] = R > i ? Math.min(pArr[2 * C - i], R - i) : 1;
             // 两个O(1)的逻辑统一到while里 它们两种情况进去直接退出来 这样省代码
             while (i + pArr[i] < str.length && i - pArr[i] > -1) {
                 if (str[i + pArr[i]] == str[i - pArr[i]]) {
@@ -49,7 +60,6 @@ public class Manacher {
                     break;
                 }
             }
-
             // 扩成功
             if (i + pArr[i] > R) {
                 C = i;
@@ -60,18 +70,46 @@ public class Manacher {
         return max - 1;
     }
 
-    public static char[] manacherString(String s) {
-        char[] charArr = s.toCharArray();
-        char[] res = new char[s.length() * 2 + 1];
 
-
-
-
-
-        return res;
-
+    // for test
+    public static int right(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] str = manacherString(s);
+        int max = 0;
+        for (int i = 0; i < str.length; i++) {
+            int L = i - 1;
+            int R = i + 1;
+            while (L >= 0 && R < str.length && str[L] == str[R]) {
+                L--;
+                R++;
+            }
+            max = Math.max(max, R - L - 1);
+        }
+        return max / 2;
     }
 
 
+    // 生成随机String的方法
+    public static String getRandomString(int possibilities, int size) {
+        char[] ans = new char[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (char) ((int) (Math.random() * possibilities) + 'a');
+        }
+        return String.valueOf(ans);
+    }
+
+    public static void main(String[] args) {
+        int possibilities = 5;
+        int setSize = 20;
+        int testTimes = 100000;
+        for (int i = 0; i < testTimes; i++) {
+            String s = getRandomString(possibilities, setSize);
+            if (manacher(s) != right(s)) {
+                System.out.println("Oops!");
+            }
+        }
+    }
 
 }
