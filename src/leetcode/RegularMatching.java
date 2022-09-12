c7980daae8f91ec31adff7645626acac42f5228e
tingxuanhu <paperplant2022@gmail.com>
2022-09-12 23:15:40 +0800

    update

--- src/leetcode/RegularMatching.java
+++ src/leetcode/RegularMatching.java
@@ -7,9 +7,6 @@ package leetcode;
 
 public class RegularMatching {
 
-}
-
-class Solution {
     public boolean isMatch(String s, String p) {
         if (s == null || p == null) {
             return false;
@@ -23,58 +20,67 @@ class Solution {
     }
 
     // compare str[si..] and pattern[pi..]
-    // prerequest:  pattern[pi] != '*'   --> cannot compare from an independent char with *
+    // pre-request:  pattern[pi] != '*'   --> cannot compare from an independent char with *
     public static boolean process(char[] str, char[] pattern, int si, int pi) {
-        // base case 1   when str come to the end  str[si..] -> " "
+        // base case 1   when str come to the end  str[si..] -> ""
         if (si == str.length) {
-            // match " "
-            if (pi == pattern.length) {
+            if (pi == pattern.length) {  // pattern[pi..] -> ""
                 return true;
             }
             if (pi + 1 < pattern.length && pattern[pi + 1] == '*') {
                 return process(str, pattern, si, pi + 2);
             }
+            return false;
         }
-        // base case 2   when pattern come to the end   pattern[pi..] -> " "
-        if (pi == str.length) {
+        // base case 2   when pattern come to the end   pattern[pi..] -> ""
+        if (pi == pattern.length) {
             return si == str.length;
         }
+
         // si < str.length && pi < pattern.length  (common positions)
         // sub-case1  pattern[pi + 1] != '*'    ==> si should match pi
         if (pi + 1 >= pattern.length || pattern[pi + 1] != '*') {
             return ((str[si] == pattern[pi]) || (pattern[pi] == '.')) && process(str, pattern, si + 1, pi + 1);
+
         }
 
-
         // sub-case2-1 pattern[pi + 1] == '*'  whereas si dose not match pi
-        if (str[si] != pattern[pi] && pattern[i] != '.') {
+        if (str[si] != pattern[pi] && pattern[pi] != '.') {
             return process(str, pattern, si, pi + 2);
         }
 
-        // sub-case2-2 pattern[pi + 1] == '*'  whereas  si matches pi
+        // sub-case2-2 pattern[pi + 1] == '*' and si matches pi
         // e.g. str = [aaa]   pattern=[.*]  --> 3a   str = [aab]   --> 2a   try all the possible cases
+        if (process(str, pattern, si, pi + 2)) {
+            return true;
+        }
 
+        while (si < str.length && (str[si] == pattern[pi] || pattern[pi] == '.')) {
+            if (process(str, pattern, si + 1, pi + 2)) {
+                return true;
+            }
+            si++;
+        }
 
-
+        return false;
     }
 
     // " " can be handled by the following method(skip all for loops), however, null cannot be processed successfully
-    public static boolean isValid(char[] s, char[] p) {
+    public static boolean isValid(char[] str, char[] pattern) {
         // str cannot include '.' or '*'
-        for (char : s) {
-            if (char == '.' || char == '*') {
+        for (char cha : str) {
+            if (cha == '.' || cha == '*') {
                 return false;
             }
         }
 
         // '*' cannot be the first element or followed by itself
-        for (int i = 1; i < p.length; i++) {
-            if (p[i] == '*' && (i == 0 || p[i - 1] == '*')) {
+        for (int i = 0; i < pattern.length; i++) {
+            if (pattern[i] == '*' && (i == 0 || pattern[i - 1] == '*')) {
                 return false;
             }
         }
         return true;
     }
-
 
 }
